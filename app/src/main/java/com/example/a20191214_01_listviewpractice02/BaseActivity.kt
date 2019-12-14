e8d085f12516b32d14bbe1b88e27555868b04c57
hyun <rlaalgus132@naver.com>
2019-12-14 11:16:10 +0900

    BaseActivity 생성 - MainActivity 상속 / onCreate 부분 정리

---
+++ app/src/main/java/com/example/a20191214_01_listviewpractice02/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a20191214_01_listviewpractice02
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
